59675ab2f1f9b1c913f3541f352b5e8b29f0d0d5
edvxndxvi <edvandavi1@hotmail.com>
2025-03-24 22:48:19 -0300

    Instalando dependencias (Lombok, JPA e H2 Database) e refatorando o projeto utilizando-as

--- src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
@@ -1,10 +1,10 @@
 package br.com.fiap.money_flow_api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,58 +13,64 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.money_flow_api.model.Category;
+import br.com.fiap.money_flow_api.repository.CategoryRepository;
 
-@RestController // component
+@RestController 
+@RequestMapping("/categories")
 public class CategoryController {
     private Logger log = LoggerFactory.getLogger(getClass());
-    // Cria uma lista que vai salvar os posts
-    private List<Category> repository = new ArrayList<>();
+    
+    // Injeção de dependência
+    @Autowired 
+    // Cria uma lista que vai salvar as categorias
+    private CategoryRepository repository;
 
     // Listar todas as categorias
     // GET :8080/categories -> retorna cógigo 200 (OK) -> json
-    @GetMapping("/categories") // Mapeia a requisição para o método, dizendo o método e seu endpoint (caminho)
+    @GetMapping // Mapeia a requisição para o método, dizendo o método e seu endpoint (caminho)
     public List<Category> index() { 
         // Vamos retornar um objeto JAVA para ser convertido em JSON
-        return repository;
+        return repository.findAll();
     }
 
     // Cadastrar categorias
     // POST
-    @PostMapping("/categories")
+    @PostMapping
     public ResponseEntity<Category> create(@RequestBody Category category){ // ResponseEntity possibilita controlar o retorno da requisição
         log.info("Cadastrando..." + category.getName());
-        repository.add(category);
+        repository.save(category);
         return ResponseEntity.status(201).body(category);
     }
 
     // Detalhes de uma categoria
-    @GetMapping("/categories/{id}")
+    @GetMapping("/{id}")
     public Category get(@PathVariable Long id){
         log.info("Buscando categoria " + id);
         return getCategory(id);
     }
     
     // Apagar categorias
-    @DeleteMapping("/categories/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void destroy(@PathVariable Long id){
         log.info("Apagando categoria " + id);
-        repository.remove(getCategory(id));
+        repository.delete(getCategory(id));
     }
 
     // Editar categorias
-    @PutMapping("/categories/{id}")
+    @PutMapping("/{id}")
     public Category update(@PathVariable Long id, @RequestBody Category category){
         log.info("Atualizando categoria " + id + " " + category);
 
-        repository.remove(getCategory(id));
+        getCategory(id);
         category.setId(id);
-        repository.add(category);
+        repository.save(category);
 
         return category; // 204
     }
@@ -72,9 +78,7 @@ public class CategoryController {
     private Category getCategory(Long id) {
         // STREAM divide cada um dos itens da lista para serem consumidos posteriormente, permitindo aplicar operações funcionais, como as seguintes
         return repository
-            .stream()
-            .filter(c -> c.getId().equals(id)) // Verifica se o 'id' de cada elemento é igual ao 'id' informado no parâmetro.
-            .findFirst()// Retorna um optional de categoria (empty), porem ainda nao retorna a categoria, ainda precisa ser manipulado
+            .findById(id)
             .orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
             );
--- src/main/java/br/com/fiap/money_flow_api/model/Category.java
+++ src/main/java/br/com/fiap/money_flow_api/model/Category.java
@@ -2,44 +2,20 @@ package br.com.fiap.money_flow_api.model;
 
 import java.util.Random;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+// Anotações JPA (@Entity e @Id) são usadas para mapear a classe Category como uma entidade no banco de dados. 
+@Entity
+// Anotações Lombok @Getter e @Setter sao usados para gerar os getters e setters. 
+@Data
 public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String icon;
 
-    public Category(Long id, String name, String icon) {
-        this.id = (id == null) ? Math.abs(new Random().nextLong()) : id; // Retorna um id caso não tenha sido informado
-        this.name = name;
-        this.icon = icon;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    @Override
-    public String toString() {
-        return "Category [id=" + id + ", name=" + name + ", icon=" + icon + "]";
-    }
-    
 }
